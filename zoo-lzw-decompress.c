50958e5f9cb4e95c05162bb7d23c9825b7a0e445
Lassi Kortela <lassi@lassi.io>
2019-09-27 22:57:38 +0300

    Change comments style

--- zoo-lzw-decompress.c
+++ zoo-lzw-decompress.c
@@ -1,8 +1,6 @@
-/*
-Lempel-Ziv decompression.  Mostly based on Tom Pfau's assembly language
-code.  The contents of this file are hereby released to the public domain.
-                                 -- Rahul Dhesi 1986/11/14, 1987/02/08
-*/
+// Lempel-Ziv decompression. Mostly based on Tom Pfau's assembly
+// language code. The contents of this file are hereby released to the
+// public domain. -- Rahul Dhesi 1986/11/14, 1987/02/08
 
 #include "func.h"
 #include "options.h"
@@ -12,10 +10,10 @@ code.  The contents of this file are hereby released to the public domain.
 #define MEMERR 2
 #define IOERR 1
 #define MAXBITS 13
-#define CLEAR 256      /* clear code */
-#define Z_EOF 257      /* end of file marker */
-#define FIRST_FREE 258 /* first free code */
-#define MAXMAX 8192    /* max code + 1 */
+#define CLEAR 256       // clear code
+#define Z_EOF 257       // end of file marker
+#define FIRST_FREE 258  // first free code
+#define MAXMAX 8192     // max code + 1
 
 #define SPARE 5
 
@@ -47,7 +45,7 @@ int push(ch) int ch;
 char out_buf_adr[IN_BUF_SIZE + SPARE];
 char in_buf_adr[OUT_BUF_SIZE + SPARE];
 
-char memflag = 0; /* memory allocated? flag */
+char memflag = 0;  // memory allocated? flag
 
 unsigned cur_code;
 unsigned old_code;
@@ -66,10 +64,10 @@ unsigned output_offset;
 int in_han, out_han;
 
 int lzd(input_handle, output_handle) int input_handle,
-output_handle; /* input & output file handles */
+output_handle;  // input & output file handles
 {
-    in_han = input_handle;   /* make it avail to other fns */
-    out_han = output_handle; /* ditto */
+    in_han = input_handle;    // make it avail to other fns
+    out_han = output_handle;  // ditto
     nbits = 9;
     max_code = 512;
     free_code = FIRST_FREE;
@@ -88,7 +86,7 @@ output_handle; /* input & output file handles */
     if (read(in_han, in_buf_adr, INBUFSIZ) == -1)
         return (IOERR);
 
-    init_dtab(); /* initialize table */
+    init_dtab();  // initialize table
 
 loop:
     cur_code = rd_dcode();
@@ -113,14 +111,14 @@ loop:
     }
 
     in_code = cur_code;
-    if (cur_code >= free_code) { /* if code not in table (k<w>k<w>k) */
-        cur_code = old_code;     /* previous code becomes current */
+    if (cur_code >= free_code) {  // if code not in table (k<w>k<w>k)
+        cur_code = old_code;      // previous code becomes current
         push(fin_char);
     }
 
-    while (cur_code > 255) {             /* if code, not character */
-        push(table[cur_code].z_ch);      /* push suffix char */
-        cur_code = table[cur_code].next; /* <w> := <w>.code */
+    while (cur_code > 255) {              // if code, not character
+        push(table[cur_code].z_ch);       // push suffix char
+        cur_code = table[cur_code].next;  // <w> := <w>.code
     }
 
     k = fin_char = cur_code;
@@ -132,17 +130,17 @@ loop:
     old_code = in_code;
 
     goto loop;
-} /* lzd() */
+}  // lzd()
 
-/* rd_dcode() reads a code from the input (compressed) file and returns
-its value. */
+// rd_dcode() reads a code from the input (compressed) file and returns its
+// value.
 unsigned rd_dcode()
 {
-    register char *ptra, *ptrb; /* miscellaneous pointers */
-    unsigned word;              /* first 16 bits in buffer */
+    register char *ptra, *ptrb;  // miscellaneous pointers
+    unsigned word;               // first 16 bits in buffer
     unsigned byte_offset;
-    char nextch;         /* next 8 bits in buffer */
-    unsigned ofs_inbyte; /* offset within byte */
+    char nextch;          // next 8 bits in buffer
+    unsigned ofs_inbyte;  // offset within byte
 
     ofs_inbyte = bit_offset % 8;
     byte_offset = bit_offset / 8;
@@ -153,9 +151,9 @@ unsigned rd_dcode()
 
         bit_offset = ofs_inbyte + nbits;
         space_left = INBUFSIZ - byte_offset;
-        ptrb = byte_offset + in_buf_adr; /* point to char */
+        ptrb = byte_offset + in_buf_adr;  // point to char
         ptra = in_buf_adr;
-        /* we now move the remaining characters down buffer beginning */
+        // we now move the remaining characters down buffer beginning
         while (space_left > 0) {
             *ptra++ = *ptrb++;
             space_left--;
@@ -166,8 +164,8 @@ unsigned rd_dcode()
         byte_offset = 0;
     }
     ptra = byte_offset + in_buf_adr;
-    /* NOTE:  "word = *((int *) ptra)" would not be independent of byte order.
-     */
+    // NOTE:  "word = *((int *) ptra)" would not be independent of byte order.
+
     word = (unsigned char)*ptra;
     ptra++;
     word = word | ((unsigned char)*ptra) << 8;
@@ -175,13 +173,13 @@ unsigned rd_dcode()
 
     nextch = *ptra;
     if (ofs_inbyte != 0) {
-        /* shift nextch right by ofs_inbyte bits */
-        /* and shift those bits right into word; */
+        // shift nextch right by ofs_inbyte bits
+        // and shift those bits right into word;
         word =
         (word >> ofs_inbyte) | (((unsigned)nextch) << (16 - ofs_inbyte));
     }
     return (word & masks[nbits]);
-} /* rd_dcode() */
+}  // rd_dcode()
 
 int init_dtab()
 {
@@ -192,28 +190,28 @@ int init_dtab()
 
 int wr_dchar(ch) char ch;
 {
-    if (output_offset >= OUTBUFSIZ) { /* if buffer full */
+    if (output_offset >= OUTBUFSIZ) {  // if buffer full
         if (out_han != -2) {
             if (write(out_han, out_buf_adr, output_offset) != output_offset)
                 prterror('f', "Write error in lzd:wr_dchar\n", (char *)0,
                          (char *)0);
         }
-        addbfcrc(out_buf_adr, output_offset); /* update CRC */
-        output_offset = 0;                    /* restore empty buffer */
+        addbfcrc(out_buf_adr, output_offset);  // update CRC
+        output_offset = 0;                     // restore empty buffer
     }
-    out_buf_adr[output_offset++] = ch; /* store character */
-} /* wr_dchar() */
+    out_buf_adr[output_offset++] = ch;  // store character
+}  // wr_dchar()
 
-/* adds a code to table */
+// adds a code to table
 int ad_dcode()
 {
-    table[free_code].z_ch = k;        /* save suffix char */
-    table[free_code].next = old_code; /* save prefix code */
+    table[free_code].z_ch = k;         // save suffix char
+    table[free_code].next = old_code;  // save prefix code
     free_code++;
     if (free_code >= max_code) {
         if (nbits < MAXBITS) {
             nbits++;
-            max_code = max_code << 1; /* double max_code */
+            max_code = max_code << 1;  // double max_code
         }
     }
 }
